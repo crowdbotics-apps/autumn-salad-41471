4cc31661edf3c1d7f729c4f7f31968aea607a600
Crowdbotics <team@crowdbotics.com>
2023-06-14 12:27:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "autumn-salad-41471",
+  url: "https://autumn-salad-41471.botics.co",
+  api: "https://autumn-salad-41471.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
